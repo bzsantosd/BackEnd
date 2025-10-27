5f488ff99ca70a9fef001bc2491c71d28f11ee8c
bzsantosd <bdossantos626@gmail.com>
2025-10-27 14:38:16 -0300

    27.10.2025

--- Aula_12/index.php
+++ Aula_12/index.php
@@ -6,18 +6,52 @@ require_once 'AlunoDAO.php';
 $dao = new AlunoDAO(); // objeto da classe Aluno DAO para testar metodos CRUD
 
 //CREATE
+
 $dao -> criarAlunos(new aluno(1, "Josias", "Panificação"));
 $dao -> criarAlunos(new aluno (2, "Victor Hugo", "Manicure"));
 $dao -> criarAlunos(new aluno(3, "Beatriz", "Eletricista"));
 
+
+// crie mais 6 objetos:
+// id - nome- curso
+// 4 - Aurora - Arquitetura
+// 5 - Oliver - Gestão
+// 6 - Amanda - Luta
+// 7 - Geysa - Engenharia
+// 8 - Joab - Eletrica
+// 9 - Miguel - Streamer
+
+$dao -> criarAlunos(new aluno(4, "Aurora", "Arquitetura"));
+$dao -> criarAlunos(new aluno(5,"Oliver", "Gestão"));
+$dao -> criarAlunos(new aluno(6,"Amanda", "Luta"));
+$dao -> criarAlunos(new aluno(7,"Geysa", "Engenharia"));
+$dao -> criarAlunos(new aluno(8,"Joab", "Eletrica"));
+$dao -> criarAlunos(new aluno(9, "Miguel", "Streamer"));
+
+
 //READ
+
 echo "\nListagem inicial:\n";
 foreach ($dao->lerAlunos() as $aluno){
     echo "{$aluno->getid()} - {$aluno->getnome()} - {$aluno->getcurso()}\n";
 }
 
 //UPDATE
+
 $dao->atualizarAlunos(1, "Jozias", "Técnico em Borracharia");
+
+// faça as seguintes atualizações
+// - Alterar nome da Geysa para Clotilde
+// - Alterar nome do Joab para Joana (operou)
+// - Alterar curso do Miguel para Designer
+// - Alterar curso da Amanda para Logistica 
+// - Alterar curso do Oliver para Eletrica
+
+$dao->atualizarAlunos(7, "Clotilde", "Engenharia");
+$dao->atualizarAlunos(8, "Joana", "Eletrica");
+$dao->atualizarAlunos(9, "Miguel", "Designer");
+$dao->atualizarAlunos(6, "Amanda", "Logistica");
+$dao->atualizarAlunos(5, "Oliver", "Eletrica");
 
 echo "\nApós Atualização:\n";
 foreach ($dao->lerAlunos() as $aluno){
@@ -25,7 +59,22 @@ foreach ($dao->lerAlunos() as $aluno){
 }
 
 //DELETE
+
+// Exclua a Clotilde e a Aurora 
+
 $dao->excluirAlunos(1); 
+echo "\nApós Exclusão:\n";
+foreach ($dao->lerAlunos() as $aluno){
+    echo "{$aluno->getid()} - {$aluno->getnome()} - {$aluno->getcurso()}\n";
+}
+
+$dao->excluirAlunos(7);
+echo "\nApós Exclusão:\n";
+foreach ($dao->lerAlunos() as $aluno){
+    echo "{$aluno->getid()} - {$aluno->getnome()} - {$aluno->getcurso()}\n";
+}
+
+$dao->excluirAlunos(4);
 echo "\nApós Exclusão:\n";
 foreach ($dao->lerAlunos() as $aluno){
     echo "{$aluno->getid()} - {$aluno->getnome()} - {$aluno->getcurso()}\n";
---
+++ Aula_13/array.php
@@ -0,0 +1,32 @@
+<?php
+
+    // Array Associativo
+
+// Diferentemente dos arrays convencionais que associam os valores armazenados no array a um índice, nos arrays associativos os valores são associados a uma chave, aonde as chaves assumem o papel do índice do array. Exemplo:
+
+    // $alunos ["nome"] = "Gabrielson";
+
+// Neste exemplo o valor "Gabrielson" está associado à chave "nome".
+
+// Isso é feito para melhorar o entendimento do código, na execução e manipulação do array. Caso fossemos trabalhar com um array convencional, teriamos que lembrar qual o índice que iria corresponder a chave "nome".
+
+
+//--------------------------------------------------------------------------
+
+    // Formato do arquivo JSON
+
+// $dao -> criarAluno (new aluno ("1", "João", "Dev"));
+// $dao -> criarAluno (new aluno ("2", "Maria", "Mecânica"));
+
+    // JSON de $alunos[];
+
+// [{ "id:" 1,
+// "nome:" "João",
+// "curso:" "Dev",
+// };
+// { "id:" 2,
+// "nome:" "Maria",
+// "curso:" "Mecânica",
+// }
+// ];
+
