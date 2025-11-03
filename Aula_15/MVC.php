333fc4fd9b0084bc2358afd7b9f4ca981c0dc4ae
bzsantosd <bdossantos626@gmail.com>
2025-11-03 16:55:49 -0300

    03.11.2025

---
+++ Aula_15/MVC.php
@@ -0,0 +1,16 @@
+<?php
+
+// o modelo MVC para criação de sistemas
+
+// o que é o padrão MVC?
+
+// este padrao é utilizado para o desenvolvimento limpo e organizado de aplicacoes.
+
+// M -> Model: representa os dados e as regras de negocio.
+// ex: Aluno.php(DRUD) e AlunoDAO.php
+
+// V -> View: cuida da parte grafica e da interface com o usuario, ou seja, o que aparece na tela. 
+// ex: Index.html ou index.php
+
+// C -> Controller: é o intermediario entre a interface e os dados e regras de negocio. 
+// ex: AlunoController.php
